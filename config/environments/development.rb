d4786c8e50151613548bc137df133fb182d8062c
Ivo Pavlov <pavlov.ivo@gmail.com>
2023-02-05 14:56:33 -0800

    add `ActionMailer` config for MailCatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,10 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # This is for mail catcher, to send mail and catch in local dev without
+  # needing a service
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.raise_delivery_errors = false
 end
