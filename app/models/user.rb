244ffacfbc02ab22ecf4233ab189fd906905cbd9
Ivo Pavlov <pavlov.ivo@gmail.com>
2023-02-05 16:10:24 -0800

    valid email check

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,14 @@ class User < ApplicationRecord
 	has_secure_password
 
 	validates :email, presence: true, uniqueness: true
+	validate :valid_email
+
 	validates :password, presence: true
 	validates :username, presence: true, uniqueness: true
+
+	def valid_email
+		unless email =~ /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+      errors.add(:email, ": #{email} is not a valid email")
+    end
+	end
 end
