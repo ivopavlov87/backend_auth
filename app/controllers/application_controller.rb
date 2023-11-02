dc44ba81dd8789263219435d55e4c6cd7d1f10c6
Ivo Pavlov <pavlov.ivo@gmail.com>
2023-11-02 16:58:36 -0700

    adding some log info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,18 @@ class ApplicationController < ActionController::API
   include JsonWebToken
 
   before_action :authenticate_request
+  before_action :set_request_id_in_thread_local
+
+  def set_request_id_in_thread_local
+    Thread.current.thread_variable_set(
+      "request_id", request.request_id
+    )
+  end
+
+  def append_info_to_payload(payload)
+    super
+    payload[:request_id] = request.request_id
+  end
 
   private
 
