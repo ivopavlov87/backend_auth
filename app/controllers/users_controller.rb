3033d93a9192d9da7050f0ca83cadd674e06e3ed
Ivo Pavlov <pavlov.ivo@gmail.com>
2024-07-13 19:42:27 -0700

    security patches

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,7 @@ class UsersController < ApplicationController
 	# GET /users
 	def index
 		@users = User.all
-		render json: @users, status: :ok
-	end
+		render json: @users, status: :ok end
 
 	# GET /users/:id
 	def show
